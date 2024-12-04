f6cb31c4e01fbf910d22c23e546052e06a7d1626
Dave Lafferty <dlits@rit.edu>
2024-12-04 15:06:47 -0500

    Updated ci/cd to lower dind mtu Replaced favicon Added feedback url

--- src/certificat/urls.py
+++ src/certificat/urls.py
@@ -1,6 +1,7 @@
 from django.urls import include, path
 import inject
 from .settings import ApplicationSettings
+from django.views.generic.base import RedirectView
 
 app_settings = inject.instance(ApplicationSettings)
 
@@ -8,6 +9,13 @@ urlpatterns = [
     path("", include("certificat.modules.html.urls")),
     path("", include("certificat.modules.api.urls")),
     path("", include("certificat.modules.acme.urls")),
+    path(
+        "feedback",
+        RedirectView.as_view(
+            url="https://forms.gle/Mm4mLqscLJ4XwQnN7", permanent=False
+        ),
+        name="feedback",
+    ),
 ]
 
 if app_settings.login_method == "saml":

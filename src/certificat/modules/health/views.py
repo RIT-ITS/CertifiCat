61c993ea3d07af7eb750aab0028574c90ad68df3
Dave Lafferty <dlits@rit.edu>
2025-10-27 13:06:26 -0400

    Fixed healthz ip restriction...

--- src/certificat/modules/health/views.py
+++ src/certificat/modules/health/views.py
@@ -11,14 +11,20 @@ logger = logging.getLogger(__name__)
 
 def can_access_healthcheck(request) -> bool:
     app_settings = inject.instance(ApplicationSettings)
+    x_forwarded_for = request.META.get("HTTP_X_FORWARDED_FOR")
+    if x_forwarded_for:
+        request_ip = x_forwarded_for.split(",")[0]
+    else:
+        request_ip = request.META.get("REMOTE_ADDR")
+
     for ip in app_settings.healthcheck_allowed_networks:
         if ip == "*":
             return True
 
-        ip_address = ipaddress.ip_address(request.META["REMOTE_ADDR"])
+        request_ip_address = ipaddress.ip_address(request_ip)
         network = ipaddress.ip_network(ip, strict=False)
 
-        if ip_address in network:
+        if request_ip_address in network:
             return True
 
     return False
